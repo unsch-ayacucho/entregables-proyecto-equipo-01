44fbcbd98e1b172767f8835f40218790950ad9d8
Pelayo27 <47894228+Pelayo27@users.noreply.github.com>
2019-05-28 07:50:29 -0500

    commit

---
+++ src/main/java/edu/pe/unsch/controller/HomeController.java
@@ -0,0 +1,14 @@
+package edu.pe.unsch.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class HomeController {
+	@GetMapping({"/", "/home"})
+	//@RequestMapping(value = "/home", method = RequestMethod.GET)
+	public String index() {
+	return "views/public/home/index";
+	}
+	
+
+}
